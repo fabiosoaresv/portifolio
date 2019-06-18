d53b3b27597ea8f0e66fbbe0bf61b937fbefb4bf
Fabio Soares <fabiosoares@outlook.com.br>
2019-06-18 03:27:19 -0300

    Update css style to render better in mobile

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,35 +5,32 @@ import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 class Contact extends Component {
   render() {
     return(
-      <div className="contact-body">
-        <Grid className="contact-grid">
-          <Cell col={6}>
-            <h2>FABIO SOARES</h2>
-            <img
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img style={{width: 270}, {height: 270}, {borderRadius: 150 / 1.5}}
               src="https://scontent.fmii3-1.fna.fbcdn.net/v/t1.0-9/45320276_1762700573859676_3368581449123889152_n.jpg?_nc_cat=105&_nc_oc=AQkudEa7nuMOPa-uUpkcE3NoU52nO1O3wLXM9rxdII6i0GGWxGfbH-sEE0hy_HA2vTc&_nc_ht=scontent.fmii3-1.fna&oh=facd54ccc66495377683bf13becc80ae&oe=5D87F6E9"
               alt="avatar"
-              style={{height: '200px'}, {borderRadius: 150 / 1.5}} />
-          </Cell>
-          <Cell col={6}>
-            <h2>Contact Me</h2>
-            <hr/>
+              className="avatar-img"
+              />
+
+            <div className="banner-text">
 
             <div className="contact-list">
-              <List>
-                <ListItem>
-                  <ListItemContent style={{fontSize: '40px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-phone-square" aria-hidden="true"/>
-                    +55 11 95916-4508
-                  </ListItemContent>
+            <h1>Contato</h1>
+            <hr/>
+            <List>
 
-                </ListItem>
-                <ListItem>
-                  <ListItemContent style={{fontSize: '26px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    fabiosoares@outlook.com.br
-                  </ListItemContent>
-                </ListItem>
-              </List>
+            <h2><i className="fa fa-phone-square" aria-hidden="true"/>
+            +55 11 95916-4508</h2>
+            <h3><i className="fa fa-envelope" aria-hidden="true"/>
+            fabiosoares@outlook.com.br</h3>
+
+            </List>
+
+            </div>
+
+
             </div>
           </Cell>
         </Grid>
@@ -41,5 +38,4 @@ class Contact extends Component {
     )
   }
 }
-
 export default Contact;
