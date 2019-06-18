3525d30a90a9061c002e1969c4c6a19502e58a27
Fabio Soares <fabiosoares@outlook.com.br>
2019-06-18 04:12:42 -0300

    Add footer to project

--- src/App.js
+++ src/App.js
@@ -8,30 +8,34 @@ class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">FABIO SOARES</Link>} scroll>
-            <Navigation>
-                <Link to="/">Home</Link>
-                <Link to="/resume">About me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">FABIO SOARES</Link>}>
+        <Layout>
+          <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">FABIO SOARES</Link>} scroll>
             <Navigation>
               <Link to="/">Home</Link>
               <Link to="/resume">About me</Link>
               <Link to="/projects">Projects</Link>
               <Link to="/contact">Contact</Link>
             </Navigation>
-        </Drawer>
-        <Content>
+          </Header>
+
+          <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">FABIO SOARES</Link>}>
+            <Navigation>
+              <Link to="/">Home</Link>
+              <Link to="/resume">About me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+
+          <Content>
             <div className="page-content" />
             <Main/>
-        </Content>
-    </Layout>
-</div>
-
+            <footer className="footer-color">
+              <p>developed by <a href="https://github.com/fabiosoaresv" rel="noopener noreferrer" target="_blank">@fabiosoaresv</a></p>
+            </footer>
+          </Content>
+        </Layout>
+      </div>
     );
   }
 }
